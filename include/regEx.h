8747a06cd8d901eb887835636ee9e9253e6ff9fd
Ankit Kisi <kisi7ankit@gmail.com>
2024-08-01 11:03:08 +0545

    final adjust

--- include/regEx.h
+++ include/regEx.h
@@ -13,11 +13,11 @@ class regEx {
 
   // Method to take input from the terminal
   std::string inputFromTerminal();
+  void checkRegularExpression(const std::string& pattern);
 
   static int precedence(char op);
   static std::string infixToPostfix(const std::string& exp);
   static std::string insertDots(const std::string& input);
 };
-// bool isValidCharacter(char c);
-void checkRegularExpression(const std::string& pattern);
+
 #endif  // UTILS_H
